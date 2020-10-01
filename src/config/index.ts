bd47140a762cd591e93b935e8808c4bae7a8e7d8
Brian Min <bymi15@yahoo.com>
2020-10-01 20:02:22 +0100

    fix(config): remove .env check

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,10 +2,7 @@ import dotenv from 'dotenv';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-const envFound = dotenv.config();
-if (envFound.error) {
-  throw new Error("Couldn't find .env file!");
-}
+dotenv.config();
 
 let databaseUrl = process.env.MONGODB_URI;
 if (process.env.NODE_ENV === 'test') {
