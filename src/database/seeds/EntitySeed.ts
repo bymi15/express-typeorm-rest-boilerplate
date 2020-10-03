0429230aad24c20e3a1538653f0d644e65211203
Brian Min <bymi15@yahoo.com>
2020-10-03 16:15:22 +0100

    feat(EntitySeed): add generic entity seeder

---
+++ src/database/seeds/EntitySeed.ts
@@ -0,0 +1,27 @@
+import { MongoRepository } from 'typeorm';
+import { Factory } from '../../types';
+
+export default class EntitySeed<Entity> {
+  private repo: MongoRepository<Entity>;
+  private factory: Factory<Entity>;
+
+  constructor(repo: MongoRepository<Entity>, factory: Factory<Entity>) {
+    this.repo = repo;
+    this.factory = factory;
+  }
+
+  public async seedOne(data?: Entity): Promise<Entity> {
+    const ent = await this.factory(data);
+    await this.repo.save(ent);
+    return ent;
+  }
+
+  public async seedMany(amount: number): Promise<Entity[]> {
+    const res: Entity[] = [];
+    for (let i = 0; i < amount; i++) {
+      res[i] = await this.factory();
+      await this.repo.save(res[i]);
+    }
+    return res;
+  }
+}
