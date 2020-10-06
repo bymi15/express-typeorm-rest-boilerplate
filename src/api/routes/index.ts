41b4dcbab86eb72d690c738fd44458e8cba438f7
Brian Min <bymi15@yahoo.com>
2020-10-06 18:55:08 +0100

    feat(jobApplicationRoute): add jobApplication endpoints

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import auth from './auth';
 import user from './user';
 import company from './company';
+import jobApplication from './jobApplication';
 
 const routes = Router();
 
 routes.use('/auth', auth);
 routes.use('/user', user);
 routes.use('/company', company);
+routes.use('/jobapplication', jobApplication);
 
 export default routes;
