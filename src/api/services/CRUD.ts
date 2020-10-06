319588cbd1e196f7157fb26436db497ad01f576e
Brian Min <bymi15@yahoo.com>
2020-10-06 15:11:24 +0100

    feat(CRUD): add fillObjectIdField function

--- src/api/services/CRUD.ts
+++ src/api/services/CRUD.ts
@@ -1,5 +1,5 @@
 import { Service } from 'typedi';
-import { MongoRepository } from 'typeorm';
+import { MongoRepository, ObjectID } from 'typeorm';
 import { Logger } from 'winston';
 import { validate } from 'class-validator';
 
@@ -15,6 +15,23 @@ export default class CRUD<Entity> {
 
   getRepo(): MongoRepository<Entity> {
     return this.repo;
+  }
+
+  protected async fillObjectIdField(
+    entity: Entity,
+    fieldName: string,
+    fieldEntityService: CRUD<any>
+  ): Promise<void> {
+    const entityName = entity.constructor.name;
+    if (!entity) throw new Error(`${entityName} not found`);
+    if (!(fieldName in entity))
+      throw new Error(`${fieldName} does not exist in ${entityName}`);
+    entity[fieldName] = await fieldEntityService.findOne(
+      <ObjectID>entity[fieldName]
+    );
+    if (!entity[fieldName]) {
+      throw new Error(`Invalid ${fieldName}`);
+    }
   }
 
   async create(entity: Entity, identifier?: string): Promise<Entity> {
@@ -37,11 +54,11 @@ export default class CRUD<Entity> {
     return await this.repo.find();
   }
 
-  async findOne(id: string): Promise<Entity | undefined> {
+  async findOne(id: string | ObjectID): Promise<Entity | undefined> {
     return await this.repo.findOne(id);
   }
 
-  async update(id: string, newEntity: Entity): Promise<Entity> {
+  async update(id: string | ObjectID, newEntity: Entity): Promise<Entity> {
     const entity = await this.findOne(id);
     if (!entity) throw new Error('The id is invalid');
     Object.keys(newEntity).forEach((key) => {
@@ -56,7 +73,7 @@ export default class CRUD<Entity> {
     return this.repo.save(entity);
   }
 
-  async delete(id: string): Promise<void> {
+  async delete(id: string | ObjectID): Promise<void> {
     await this.repo.delete(id);
   }
 }
