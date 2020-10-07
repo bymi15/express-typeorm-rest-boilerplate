8878c14391d0915bfaf2c0ee3b2517143d73c88e
Brian Min <bymi15@yahoo.com>
2020-10-07 03:10:19 +0100

    fix(JobApplicationFactory): fix ObjectID bug with typeorm

--- src/database/factories/JobApplicationFactory.ts
+++ src/database/factories/JobApplicationFactory.ts
@@ -1,13 +1,14 @@
 import { JobApplication } from '../../api/entities/JobApplication';
 import * as faker from 'faker';
+import { ObjectId } from 'mongodb';
 import { ObjectID } from 'typeorm';
 
 export default (data?: JobApplication): JobApplication => {
   const jobApplication = new JobApplication({
     role: (data && data.role) || faker.name.jobTitle(),
     description: (data && data.description) || faker.lorem.sentences(),
-    company: (data && data.company) || new ObjectID(),
-    user: (data && data.user) || new ObjectID(),
+    company: (data && data.company) || (new ObjectId() as ObjectID),
+    user: (data && data.user) || (new ObjectId() as ObjectID),
     status:
       (data && data.status) ||
       faker.random.arrayElement(['applied', 'offer', 'accepted', 'rejected']),
