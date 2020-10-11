6a026f78cea5bb520ff2e6bb7e9c1789beb03b30
Brian Min <bymi15@yahoo.com>
2020-10-11 05:03:33 +0100

    fix(UserFactory): add user role

--- src/database/factories/UserFactory.ts
+++ src/database/factories/UserFactory.ts
@@ -1,4 +1,4 @@
-import { User } from '../../api/entities/User';
+import { User, Role } from '../../api/entities/User';
 import bcrypt from 'bcrypt';
 import * as faker from 'faker';
 
@@ -7,11 +7,17 @@ export default async (data?: User): Promise<User> => {
   if (data && data.password) {
     password = await bcrypt.hash(data.password, 12);
   }
+  const randomRole: Role = faker.random.arrayElement([
+    'user',
+    'staff',
+    'admin',
+  ]);
   const user = new User({
     firstName: (data && data.firstName) || faker.name.firstName(),
     lastName: (data && data.lastName) || faker.name.lastName(),
     email: (data && data.email) || faker.internet.email(),
     password,
+    role: (data && data.role) || randomRole,
   });
   return user;
 };
