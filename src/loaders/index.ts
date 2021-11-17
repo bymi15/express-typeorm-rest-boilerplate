b6ce7f851c9609d2e6377b874118e68558a6b9b6
Brian Min <bymi15@yahoo.com>
2021-11-17 02:51:39 +0000

    refactor(loaders): log error

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,14 +1,15 @@
-import { Container } from 'typedi';
-import expressLoader from './express';
-import databaseLoader from './database';
-import Logger from '../logger';
 import { Application } from 'express';
+import { Container } from 'typedi';
+import Logger from '../logger';
+import databaseLoader from './database';
+import expressLoader from './express';
 
 export default async (app: Application): Promise<void> => {
   Container.set('logger', Logger);
   try {
     await databaseLoader();
   } catch (err) {
+    Logger.error(err);
     throw err;
   }
   Logger.info('Database loaded and connected!');
